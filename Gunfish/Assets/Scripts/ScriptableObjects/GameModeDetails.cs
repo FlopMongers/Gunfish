cffc96730d74528cacfd904692202cd3efe95a88
Ryan Kann <ryanakann@gmail.com>
2023-04-16 13:04:59 -0400

    GameModeDetails scriptable objects

---
+++ Gunfish/Assets/Scripts/ScriptableObjects/GameModeDetails.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameModeDetails", menuName = "ScriptableObjects/Game Mode Details", order = 0)]
+public class GameModeDetails : ScriptableObject {
+    public Texture2D image;
+}
