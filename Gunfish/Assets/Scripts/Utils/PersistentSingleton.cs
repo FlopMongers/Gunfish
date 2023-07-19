0506fe21886e5687e987bb06355c7faab464f4e5
Ryan Kann <ryanakann@gmail.com>
2023-07-18 22:23:55 -0400

    Singleton formatting

--- Gunfish/Assets/Scripts/Utils/PersistentSingleton.cs
+++ Gunfish/Assets/Scripts/Utils/PersistentSingleton.cs
@@ -1,6 +1,3 @@
-/// <summary>
-/// Singleton that persists across multiple scenes
-/// </summary>
 public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T> {
 	protected override void Awake() {
 		base.Awake();
