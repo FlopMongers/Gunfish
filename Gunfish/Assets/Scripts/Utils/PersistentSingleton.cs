31963089f7b99f61b8aeaba6c0fdfcf5883af21f
Ryan Kann <ryanakann@gmail.com>
2023-07-18 22:19:58 -0400

    Update PersistentSingleton.cs
    Unity does not allow setting dontdestroyonload for non-root gameobjects.

--- Gunfish/Assets/Scripts/Utils/PersistentSingleton.cs
+++ Gunfish/Assets/Scripts/Utils/PersistentSingleton.cs
@@ -1,16 +1,9 @@
 /// <summary>
 /// Singleton that persists across multiple scenes
 /// </summary>
-public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
-{
-	public bool persistRoot;
-
-	protected override void Awake()
-	{
+public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T> {
+	protected override void Awake() {
 		base.Awake();
-		if (persistRoot)
-			DontDestroyOnLoad(transform.root.gameObject);
-		else
-			DontDestroyOnLoad(gameObject);
+		DontDestroyOnLoad(transform.root.gameObject);
 	}
 }
