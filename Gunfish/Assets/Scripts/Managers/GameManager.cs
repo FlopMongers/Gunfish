6c0438027e9977889dcb8b12313364001b4166ef
Jeffrey-Wang <17jwapples@gmail.com>
2023-08-17 23:31:42 -0400

    Add lighting to Stage0

--- Gunfish/Assets/Scripts/Managers/GameManager.cs
+++ Gunfish/Assets/Scripts/Managers/GameManager.cs
@@ -41,7 +41,7 @@ public class GameManager : PersistentSingleton<GameManager> {
             MatchManager = null;
         }
 
-        var scenes = new List<string>() { "Player Loading" };
+        var scenes = new List<string>() { "Player Loading", "Stage0" };
 
         // Get all active players
         GameParameters parameters = new GameParameters(PlayerManager.instance.Players, scenes);
