75634c73a180095d7cf8705df5e0536ad0efb956
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-09-12 11:44:22 -0400

    bezier stuff from the plane

--- Gunfish/Assets/Scripts/WaterMaterialInterface.cs
+++ Gunfish/Assets/Scripts/WaterMaterialInterface.cs
@@ -8,9 +8,11 @@ using static FunkyCode.Rendering.Universal.Sprite;
 public class WaterMaterialInterface : MonoBehaviour {
     [HideInInspector]
     public List<Transform> waterSurfaceNodes;
-    
+
     private double[] positionsX;
     private double[] positionsY;
+    private double[] positionsdX;
+    private double[] positionsdY;
 
     private double[] coefficients;
     private int degree = 10;
@@ -53,6 +55,8 @@ public class WaterMaterialInterface : MonoBehaviour {
 
         positionsX = new double[waterSurfaceNodes.Count];
         positionsY = new double[waterSurfaceNodes.Count];
+        positionsdX = new double[waterSurfaceNodes.Count];
+        positionsdY = new double[waterSurfaceNodes.Count];
 
         GetComponent<SpriteRenderer>().material.SetInt("_NodeCount", waterSurfaceNodes.Count);
         GetComponent<SpriteRenderer>().material.SetInt("_Degree", degree);
@@ -62,18 +66,20 @@ public class WaterMaterialInterface : MonoBehaviour {
         for (int i = 0; i < waterSurfaceNodes.Count; i++) {
             positionsX[i] = waterSurfaceNodes[i].position.x;
             positionsY[i] = waterSurfaceNodes[i].position.y;
+            positionsdX[i] = 0.333;
+            positionsdY[i] = 0;
 
             if (i == waterSurfaceNodes.Count - 1) {
                 continue;
             }
-            
+
             var colliders = waterSurfaceNodes[i].GetComponents<BoxCollider2D>();
             var effector = waterSurfaceNodes[i].GetComponent<BuoyancyEffector2D>();
             var waterDimensions = GetComponentInParent<WaterSurfaceGenerator>().dimensions;
 
             Vector2 offset = (waterSurfaceNodes[i + 1].position - waterSurfaceNodes[i].position);
             var surfaceMidpoint = (Vector2)waterSurfaceNodes[i].position + offset / 2;
-            
+
             var width = offset.x;
             var height = surfaceMidpoint.y - transform.parent.position.y + waterDimensions.y / 2;
 
@@ -88,6 +94,48 @@ public class WaterMaterialInterface : MonoBehaviour {
 
         GetComponent<SpriteRenderer>().material.SetFloatArray("_NodesX", positionsX.Select(x => (float)x).ToArray());
         GetComponent<SpriteRenderer>().material.SetFloatArray("_NodesY", positionsY.Select(y => (float)y).ToArray());
+        GetComponent<SpriteRenderer>().material.SetFloatArray("_NodesdX", positionsX.Select(x => (float)0.3333333).ToArray());
+        GetComponent<SpriteRenderer>().material.SetFloatArray("_NodesdY", positionsY.Select(y => (float)0).ToArray());
         GetComponent<SpriteRenderer>().material.SetFloatArray("_Coefficients", coefficients.Select(c => (float)c).ToArray());
+    }
+
+    Vector2 midpoint(Vector2 p1, Vector2 p2, float t) {
+        return Vector2.Lerp(p1,p2,t);
+    }
+
+    Vector2 cubicBezier(Vector2[] controlPoints, float t) {
+        Vector2 midpoint0 = midpoint(controlPoints[0],controlPoints[1],t);
+        Vector2 midpoint1 = midpoint(controlPoints[1],controlPoints[2],t);
+        Vector2 midpoint2 = midpoint(controlPoints[2],controlPoints[3],t);
+        Vector2 midmid0 = midpoint(midpoint0,midpoint1,t);
+        Vector2 midmid1 = midpoint(midpoint1,midpoint2,t);
+        Vector2 midmidmid = midpoint(midmid0,midmid1,t);
+        return midmidmid;
+    }
+
+    float bezierHeight(float x) {
+        int prevIndex = ClosestIndexBefore(positionsX, x);
+        int nextIndex = prevIndex+1;
+        Vector2 prev = new Vector2((float)(positionsX[prevIndex]),(float)(positionsY[prevIndex]));
+        Vector2 next = new Vector2((float)(positionsX[nextIndex]),(float)(positionsY[nextIndex]));
+        Vector2 control0 = prev;
+        Vector2 control1 = new Vector2((float)(prev.x+positionsdX[prevIndex]),(float)(prev.y+positionsdY[prevIndex]));
+        Vector2 control2 = new Vector2((float)(next.x-positionsdX[nextIndex]),(float)(next.y-positionsdY[nextIndex]));
+        Vector2 control3 = next;
+        Vector2[] controlPoints = new Vector2[4] {control0,control1,control2,control3};
+        float t = (x-prev.x)/(next.x-prev.x);
+        return cubicBezier(controlPoints,t).y;
+    }
+
+    int ClosestIndexBefore(double[] positions, float x) {
+        int beforeIndex = positions.Length - 1;
+        for (int i = 0; i < positions.Length; i++) {
+            float px = (float)positions[i];
+            if (px > x) {
+                beforeIndex = i - 1;
+                break;
+            }
+        }
+        return beforeIndex;
     }
 }
--- Gunfish/Assets/Shaders/Water.shader
+++ Gunfish/Assets/Shaders/Water.shader
@@ -15,6 +15,8 @@ Shader "Unlit/Water"
         Pass
         {
             CGPROGRAM
+// Upgrade NOTE: excluded shader from DX11 because it uses wrong array syntax (type[size] name)
+#pragma exclude_renderers d3d11
             #pragma vertex vert
             #pragma fragment frag
 
@@ -34,19 +36,56 @@ Shader "Unlit/Water"
             };
 
             fixed4 _Color;
-            float _NodesX[1000];
-            float _NodesY[1000];
-            float _Coefficients[1000];
+            float _NodesX[500];
+            float _NodesY[500];
+            float _NodesdX[500];
+            float _NodesdY[500];
+            float _Coefficients[50];
             int _NodeCount;
             int _Degree;
-            
+            int closestIndexBefore(float x){
+                int beforeIndex = _NodeCount - 1;
+                for (int i = 0; i < _NodeCount; i++) {
+                    float px = _NodesX[i];
+                    if (px > x) {
+                        beforeIndex = i - 1;
+                        break;
+                    }
+                }
+                return beforeIndex;
+            }
             float ripple(float x){
                 return  0.021*sin(5*(x-(_Time*30)))+
                         0.021*sin(11.5*(x+(_Time*25)))*sin(x+_Time*25)+
                         0.021*sin(4.6*(x-(_Time*18)))+
                         0.021*sin(2*(x+(_Time+16)));
             }
-            float evaluate(float x) {
+            float2 midpoint(float2 p1, float2 p2, float t){
+                return lerp(p1,p2,t);
+            }
+            float2 cubicBezier(float2 controlPoints[4], float t){
+                float2 midpoint0 = midpoint(controlPoints[0],controlPoints[1],t);
+                float2 midpoint1 = midpoint(controlPoints[1],controlPoints[2],t);
+                float2 midpoint2 = midpoint(controlPoints[2],controlPoints[3],t);
+                float2 midmid0 = midpoint(midpoint0,midpoint1,t);
+                float2 midmid1 = midpoint(midpoint1,midpoint2,t);
+                float2 midmidmid = midpoint(midmid0,midmid1,t);
+                return midmidmid;
+            }
+            float bezierHeight(float x){
+                int prevIndex = closestIndexBefore(x);
+                int nextIndex = prevIndex+1;
+                float4 prev = (_NodesX[prevIndex],_NodesY[prevIndex],_NodesdX[prevIndex],_NodesdY[prevIndex]);
+                float4 next = (_NodesX[nextIndex],_NodesY[nextIndex],_NodesdX[nextIndex],_NodesdY[nextIndex]);
+                float2 control0 = (prev.x, prev.y);
+                float2 control1 = (prev.x+prev[2],prev.y+prev[3]);
+                float2 control2 = (next.x-next[2],next.y-next[3]);
+                float2 control3 = (next.x, next.y);
+                float2 controlPoints[4] = {control0,control1,control2,control3};
+                float t = (x-prev.x)/(next.x-prev.x);
+                return cubicBezier(controlPoints,t).y;
+            }
+            float polynomial(float x){
                 float sum = 0;
                 for (int i = 0; i < _Degree + 1; i++) {
                     if (i >= _NodeCount)
@@ -57,7 +96,10 @@ Shader "Unlit/Water"
                     }
                     sum += _Coefficients[i] * xpow;
                 }
-                return sum + ripple(x);
+                return sum;
+            }
+            float evaluate(float x) {
+                return bezierHeight(x) + ripple(x);
             }
 
             float distanceBelowSurface(float x, float y){
