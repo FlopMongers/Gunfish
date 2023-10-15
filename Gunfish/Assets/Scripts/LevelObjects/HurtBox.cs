1a981add8c635ebd930fd875bd5445b1fd806d20
SimonMarkWhittle <simonmarkwhittle@gmail.com>
2023-10-14 21:48:21 -0400

    added broken pipe

---
+++ Gunfish/Assets/Scripts/LevelObjects/HurtBox.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FishDetector))]
+public class HurtBox : MonoBehaviour
+{
+    [SerializeField]
+    float damage;
+
+    HashSet<Gunfish> hurtFish = new HashSet<Gunfish>();
+
+    // add start method to initialize fish detector
+    public void Start() {
+        var detector = GetComponent<FishDetector>();
+        detector.OnFishTriggerEnter += HandleFishTriggerEnter;
+        detector.OnFishTriggerExit += HandleFishTriggerExit;
+    }
+
+    public void Update() {
+
+        // can't? remove dead fish while iterating collection
+        List<Gunfish> fishesToRemove = new List<Gunfish>();
+        foreach (Gunfish fish in hurtFish) {
+            fish.UpdateHealth(-damage);
+
+            if (!fish.statusData.alive) {
+                fishesToRemove.Add(fish);
+            }
+        }
+
+        // remove dead fishes from set now that we're done
+        foreach (Gunfish deadFish in fishesToRemove) {
+            hurtFish.Remove(deadFish);
+        }
+    }
+
+    protected void HandleFishTriggerEnter(GunfishSegment segment, Collider2D collider) {
+        hurtFish.Add(segment.gunfish);        
+    }
+
+    protected void HandleFishTriggerExit(GunfishSegment segment, Collider2D collider) {
+        hurtFish.Remove(segment.gunfish);
+    }
+
+}
