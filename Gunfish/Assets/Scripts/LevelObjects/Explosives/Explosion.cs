2aa5bfb1dc8307883f12b78054e5a6f6bd274167
Wyatt Joyner <wjoyner127@gmail.com>
2024-01-10 03:23:12 +0000

    explosions create sploosh

--- Gunfish/Assets/Scripts/LevelObjects/Explosives/Explosion.cs
+++ Gunfish/Assets/Scripts/LevelObjects/Explosives/Explosion.cs
@@ -9,12 +9,18 @@ public class Explosion : MonoBehaviour
     public float damageScale;
 
     public PointEffector2D effector;
+    public float explosionForce = 50;
+
+    public float waterSplooshForce = 25;
+
+    float waterScale = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         Explode();
         // screen shake
+        effector.forceMagnitude = explosionForce;
         Invoke("TurnOffEffector", 0.25f);
     }
 
@@ -26,7 +32,14 @@ public class Explosion : MonoBehaviour
         // circle cast to get things in radius
         // raycast to make sure no walls in the way
         // raycast from the things you hit and if blocked by ground, then ignore
-        int mask = ~LayerMask.GetMask("Ground");
+        int mask = LayerMask.GetMask("Water");
+        if (Physics2D.Raycast(transform.position, Vector2.up, 0.01f, mask)) {
+            damageScale *= waterScale;
+            effector.forceMagnitude *= waterScale;
+            transform.localScale = new Vector3(transform.localScale.x * waterScale, transform.localScale.y * waterScale, 1f);
+        }
+
+        mask = ~LayerMask.GetMask("Ground");
         foreach (var hit in Physics2D.CircleCastAll(transform.position, radius.bounds.extents.x, Vector2.zero, 0, mask)) {
             //print($"hit {hit.transform}");
             HashSet<GameObject> hittables = new HashSet<GameObject>();
@@ -37,6 +50,12 @@ public class Explosion : MonoBehaviour
                 hittable = segment.gunfish;
             }
             else if (hitTransform.GetComponentInParent<Shootable>()) { hittable = hitTransform.GetComponentInParent<Shootable>(); }
+            WaterSurfaceNode node = hitTransform.GetComponent<WaterSurfaceNode>();
+            if (node != null) {
+                node.Sploosh(Vector2.up * damageCurve.Evaluate(
+                        Vector3.Distance(transform.position, node.GetComponent<BoxCollider2D>().bounds.max) / radius.bounds.extents.x) * waterSplooshForce);
+            }
+                
             if (hittable == null || hittables.Contains(hittable.gameObject)) {
                 continue;
             }
