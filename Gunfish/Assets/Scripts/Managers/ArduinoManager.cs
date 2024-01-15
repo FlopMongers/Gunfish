f9cd5b3d59af68b1f66628b176eba2b2e717bc85
Ryan Kann <ryanakann@gmail.com>
2024-01-14 23:45:22 -0500

    Gunfish attractors

--- Gunfish/Assets/Scripts/Managers/ArduinoManager.cs
+++ Gunfish/Assets/Scripts/Managers/ArduinoManager.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
 using System.IO.Ports;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class ArduinoManager : Singleton<ArduinoManager> {
+
+    public List<AudioClip> attractorLines;
+    public float secondsBetweenAttractors = 60f;
+    private float secondsSinceLastAttractor;
 
     private SerialPort serialPort;
 
     private AudioClip clip;
     private AudioSource source;
     private float[] data;
+
+    private void Attractor() {
+        if (secondsSinceLastAttractor > secondsBetweenAttractors) {
+            secondsSinceLastAttractor = 0f;
+            PlayClip(attractorLines[Random.Range(0, attractorLines.Count)]);
+        } else {
+            secondsSinceLastAttractor += Time.deltaTime;
+        }
+    }
 
     public void PlayClip(AudioClip clip) {
         this.clip = clip;
@@ -21,12 +36,20 @@ public class ArduinoManager : Singleton<ArduinoManager> {
         serialPort = new SerialPort("COM3", 9600) {
             ReadTimeout = 100
         };
-
+        secondsSinceLastAttractor = 0f;
         source = GetComponent<AudioSource>();
         clip = source.clip;
         ConnectArduino();
 
         base.Initialize();
+    }
+    private void HandleArduino() {
+        if (serialPort.IsOpen) {
+            float loudness = SampleLoudness();
+            byte volume = (byte)Mathf.RoundToInt(loudness);
+            byte[] buffer = new byte[] { volume };
+            serialPort.Write(buffer, 0, 1);
+        }
     }
 
     private void ConnectArduino() {
@@ -59,16 +82,8 @@ public class ArduinoManager : Singleton<ArduinoManager> {
     }
 
     private void Update() {
-        if (serialPort.IsOpen) {
-            float loudness = SampleLoudness();
-            byte volume = (byte)Mathf.RoundToInt(loudness);
-            byte[] buffer = new byte[] { volume };
-            serialPort.Write(buffer, 0, 1);
-        }
-
-        if (!GameManager.debug) {
-            return;
-        }
+        Attractor();
+        HandleArduino();
     }
 
     protected override void OnDestroy() {
