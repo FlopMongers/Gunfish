e16588b0fe64eb50bf8688989dfe59549c55e3cc
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2024-01-17 23:33:53 -0500

    prep for the controller ordering ritual

--- Gunfish/Assets/Scripts/Managers/PlayerManager.cs
+++ Gunfish/Assets/Scripts/Managers/PlayerManager.cs
@@ -29,10 +29,23 @@ public class PlayerManager : PersistentSingleton<PlayerManager> {
         var inputDevices = InputSystem.devices.Where(device => regex.IsMatch(device.displayName)).OrderBy(device => device.deviceId).ToList();
 
         int playerIndex = 0;
-        // inputDevices = inputDevices.OrderBy(x => x.name).ToList();
-        foreach (var device in inputDevices)
-            print(device);
-        inputDevices.ForEach(device => {
+        List<InputDevice> devices = new List<InputDevice>(inputDevices);
+        List<int> deviceIdPressOrder = new List<int>() {
+            1,2,3,4
+        };
+        // List<int> deviceIdPressOrder = GameManager.Instance.controllerPressOrder;
+        if (deviceIdPressOrder.Count > 0) {
+            devices = new List<InputDevice>();
+            List<InputDevice> assignedDevices = new List<InputDevice>();
+            deviceIdPressOrder.ForEach(deviceId => {
+                inputDevices.ForEach(device => {
+                    if (device.deviceId == deviceId) {
+                        devices.Add(device);
+                    }
+                });
+            });
+        }
+        devices.ForEach(device => {
             var playerInput = inputManager.JoinPlayer(playerIndex: playerIndex, pairWithDevice: device);
             var player = playerInput.GetComponent<Player>();
             player.Initialize(playerIndex);
